de36d7937fa082b9d33dcfe4142029f0ac1a9522
Invast_97 <ustimbazarnik@gmail.com>
2025-03-23 16:13:30 +0200

    updated main

--- source/Main.java
+++ source/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        System.out.println("Hello World " + st);
     }
 }
